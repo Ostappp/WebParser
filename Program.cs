f53308969c685cc61d157b3f537e39236930f650
Ostap <ostapilienkiv@gmail.com>
2025-03-04 17:09:27 +0200

    Added Docker container

--- Program.cs
+++ Program.cs
@@ -9,31 +9,31 @@ namespace WebParser
         static async Task Main(string[] args)
         {
             Console.WriteLine("Program started...");
-            //using (StreamWriter logFile = new(Consts.LogFilePath, true))
-            //{
-            //    TeeTextWriter teeWriter = new(Console.Out, logFile);
-            //    Console.SetOut(teeWriter);
-            //    Console.SetError(teeWriter);
-            //}
-
             string url_to_parse = @"https://amountwork.com/ua/rabota/ssha/voditel";
-            string html = await HtmlLoader.GetHtmlAsync(url_to_parse);
-            AmountworkHtmlParser amountworkParser = new AmountworkHtmlParser(url_to_parse);
-            var jobUrls = await amountworkParser.GetJobsUrls(html);
-            List<JobInfoModel> models = new();
-            foreach (var url in jobUrls)
+            using (StreamWriter logFile = new(Consts.LogFilePath, true))
             {
-                models.Add(await amountworkParser.ParseUrl(url));
-            }            
-            
-            // Серіалізація масиву в JSON
-            string jsonModels = JsonConvert.SerializeObject(models, Formatting.Indented);
+                TeeTextWriter teeWriter = new(Console.Out, logFile);
+                Console.SetOut(teeWriter);
+                Console.SetError(teeWriter);
 
-            await StringObjectSaver.SaveJsonToFileAsync("jobs.json", jsonModels);
-            await StringObjectSaver.SaveToCsvAsync("jobs.csv", models);
+                string html = await HtmlLoader.GetHtmlAsync(url_to_parse);
+                AmountworkHtmlParser amountworkParser = new AmountworkHtmlParser(url_to_parse);
+                var jobUrls = await amountworkParser.GetJobsUrls(html);
+                List<JobInfoModel> models = new();
+                foreach (var url in jobUrls)
+                {
+                    models.Add(await amountworkParser.ParseUrl(url));
+                }
 
-            Console.WriteLine("Press any key to close the program");
-            Console.ReadLine();
+                // Серіалізація масиву в JSON
+                string jsonModels = JsonConvert.SerializeObject(models, Formatting.Indented);
+
+                await StringObjectSaver.SaveJsonToFileAsync(Consts.JsonVacanciesPath, jsonModels);
+                await StringObjectSaver.SaveToCsvAsync(Consts.CsvVacanciesPath, models);
+
+                Console.WriteLine("Press any key to close the program");
+            }
+
         }
     }
 }
